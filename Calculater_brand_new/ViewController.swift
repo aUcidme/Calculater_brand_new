24221c5e3f78fb376d91cf209484e77066a50be4
aUcidme <aUcid@ciddeMacBook-Pro.local>
2016-12-01 10:16:54 +0800

    Initial Commit

---
+++ Calculater_brand_new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculater_brand_new
+//
+//  Created by cid aU on 2016/12/1.
+//  Copyright © 2016年 cid aU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
